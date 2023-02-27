0184dc093d017a85408a8edab1327c696a05144c
johnathan carstens <jcarstensedu@gmail.com>
2023-02-27 11:23:27 -0700

    Complete lab, I made some small changes

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-const name = "Joe";
-const height = 74;
-const message = `${name} is ${height} inches tall`;
+const name = "Johnny";
+const heightInFeet = 6;
+const heightInInches = 2;
+const message = `${name} is ${heightInFeet} feet and ${heightInInches} inches tall`;
 
-module.exports = {name, height, message};
+module.exports = { name, heightInFeet, heightInInches, message };
+console.log(message);
